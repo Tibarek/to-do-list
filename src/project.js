98641a78c08e9f8369358dcfb34f1dbaa5e42515
Tibarek Yuggie <ttibarek@gmail.com>
2025-12-11 00:07:34 +0300

    delete tasks.js

--- src/project.js
+++ src/project.js
@@ -1,1 +1,20 @@
+class project {
+  constructor(title) {
+    this.title = title;
+    this.todo_list = [];
+  }
 
+  addTodo(item) {
+    this.todo_list.push(item);
+  }
+
+  getTodo() {
+    return this.todo_list;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+export { project };
--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,8 @@
-
+export const todo = function (title, description, dueDate, priority) {
+  return {
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    priority: priority,
+  };
+};
